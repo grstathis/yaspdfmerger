a42ca6fa1576ae224122c64db5612f9beba6a4a7
Stathis Grigoropoulos <touristroni@gmail.com>
2013-05-15 00:34:04 +0200

    initial project version

---
+++ PDFmerger/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PDFmerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.merge = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pages = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(818, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose PDF files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // merge
+            // 
+            this.merge.Location = new System.Drawing.Point(12, 119);
+            this.merge.Name = "merge";
+            this.merge.Size = new System.Drawing.Size(107, 50);
+            this.merge.TabIndex = 3;
+            this.merge.Text = "Merge!";
+            this.merge.UseVisualStyleBackColor = true;
+            this.merge.Click += new System.EventHandler(this.merge_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
+            this.Pages});
+            this.dataGridView1.Location = new System.Drawing.Point(125, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(705, 238);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Output FileName";
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FileName.FillWeight = 149.2386F;
+            this.FileName.HeaderText = "FileName";
+            this.FileName.Name = "FileName";
+            // 
+            // Pages
+            // 
+            this.Pages.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Pages.FillWeight = 50.76142F;
+            this.Pages.HeaderText = "Pages";
+            this.Pages.Name = "Pages";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.merge);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PDFmerger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1 ;
+        private System.Windows.Forms.Button merge;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pages;
+    }
+}
+
---
+++ PDFmerger/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Text.RegularExpressions;
+
+
+namespace PDFmerger
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string outfilename = "";
+        // Open the output document
+        PdfDocument outputDocument = new PdfDocument();
+        //datatable to connect to datagridview 
+        DataTable pdf_files = new DataTable();
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //pdf_files.Columns.Add("FileName", typeof(string));
+            //pdf_files.Columns.Add("Pages", typeof(string));
+           
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+	        if (result == DialogResult.OK){
+                foreach (String file in openFileDialog1.FileNames){
+                    
+                    // idx is the new index. The cells must also be accessed by an index
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[0].Value = file;
+                    
+                }
+                
+	        }
+	        
+	    }
+
+        private void merge_Click(object sender, EventArgs e)
+        {
+
+                 
+
+            
+            
+            
+            // If the file name is not an empty string display the result name in textbox for saving.
+            if (textBox2.Text != "")
+            {
+                //textBox2.Text = saveFileDialog1.FileName;
+            }
+            else
+            {
+                // Displays a SaveFileDialog so the user can save the pdf
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "PDF file|*.pdf";
+                saveFileDialog1.Title = "Save the Output File";
+                saveFileDialog1.ShowDialog();
+            }
+
+            if (File.Exists(textBox2.Text))
+            {
+                File.Delete(textBox2.Text);
+            }
+            
+
+            //Loop files selected...
+            int nrows = dataGridView1.RowCount;
+            int it = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                it++;
+                if (it == nrows) { break; }
+                string file, pages;
+                string[] spages;
+                spages = new String[1];
+                if (row.Cells[0].Value != null)
+                {
+                    file = row.Cells[0].Value.ToString();
+                }
+                else
+                {
+                    file = "";
+                    MessageBox.Show("Nothing to Merge!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    continue;
+                }
+                if (row.Cells[1].Value != null)
+                {
+                    pages = row.Cells[1].Value.ToString();
+                    pages = Regex.Replace(pages,@"\s+","");
+                    pages = Regex.Replace(pages, @"[^(0-9)+,\-].", "");
+                    pages = Regex.Replace(pages, @",+", ",");
+                    Match match = Regex.Match(pages, @",");
+                    if (match.Success)
+                    {
+                        spages = pages.Split(',');
+                    }
+                    else
+                    {
+                        spages[0] = pages;
+                    }
+                }
+                else 
+                {
+                    pages = "all";
+                }
+                   
+                if (File.Exists(file) == true){
+
+                        PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+                        int count = inputDocument.PageCount;
+                 
+                        if (pages == "all")
+                        {
+                            for (int idx = 0; idx < count; idx++)
+                            {
+                                // Get the page from the external document...
+                                PdfPage page = inputDocument.Pages[idx];
+                                // ...and add it to the output document.
+                                outputDocument.AddPage(page);
+                            }
+                        }
+                        else
+                        {
+                            foreach (string spage in spages)
+                            {
+                                if (spage != "")
+                                {
+                                    Match match = Regex.Match(spage, @"-");
+                                    if (match.Success)
+                                    {
+                                        string[] StartEndPages = spage.Split('-');
+                                        int StartPage = Convert.ToInt32(StartEndPages[0]);
+                                        int EndPage = Convert.ToInt32(StartEndPages[StartEndPages.Count() - 1]);
+                                        if (StartPage > EndPage)
+                                        {
+                                            MessageBox.Show("The page numbers in file " + file + " are incorrect placed!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                            DialogResult dialogResult = MessageBox.Show("Do you want to fix this automatically?", "Warning", MessageBoxButtons.YesNo);
+                                            if (dialogResult == DialogResult.Yes)
+                                            {
+                                                int temp = StartPage;
+                                                StartPage = EndPage;
+                                                EndPage = temp;
+                                            }
+                                            else if (dialogResult == DialogResult.No)
+                                            {
+                                                continue;
+                                            }
+
+
+                                        }
+                                        if (StartPage > count || EndPage > count)
+                                        {
+                                            MessageBox.Show("The page numbers in file " + file + " are greater than permitted!", "Warning",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                            DialogResult dialogResult = MessageBox.Show("Do you want to fix this automatically?", "Help", MessageBoxButtons.YesNo);
+                                            if (dialogResult == DialogResult.Yes)
+                                            {
+                                                if (StartPage > count)
+                                                {
+                                                    StartPage = count;
+                                                }
+                                                if (EndPage > count)
+                                                {
+                                                    EndPage = count;
+                                                }
+                                            }
+                                            else if (dialogResult == DialogResult.No)
+                                            {
+                                                MessageBox.Show("Continouning with Errors");
+                                                continue;
+
+                                            }
+                                        }
+
+
+                                        for (int idx = StartPage-1; idx <= EndPage-1; idx++)
+                                        {
+                                            // Get the page from the external document...
+                                            PdfPage page = inputDocument.Pages[idx];
+                                            // ...and add it to the output document.
+                                            outputDocument.AddPage(page);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        int StartPage = Convert.ToInt32(spage);
+                                        if (StartPage > count)
+                                        {
+                                            MessageBox.Show("The page numbers in file " + file + " are greater than permitted", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                            DialogResult dialogResult = MessageBox.Show("Do you want to fix this automatically?", "Warning", MessageBoxButtons.YesNo);
+                                            if (dialogResult == DialogResult.Yes)
+                                            {
+                                                if (StartPage > count)
+                                                {
+                                                    StartPage = count;
+                                                }
+                                            }
+                                            else if (dialogResult == DialogResult.No)
+                                            {
+                                                continue;
+                                            }
+                                        }
+                                        
+                                            for (int idx = StartPage-1; idx <= StartPage-1; idx++)
+                                            {
+                                                // Get the page from the external document...
+                                                PdfPage page = inputDocument.Pages[idx];
+                                                // ...and add it to the output document.
+                                                outputDocument.AddPage(page);
+                                            }
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(file + " does not exist!");
+                    }
+                    
+
+             }
+           
+            outfilename = textBox2.Text;
+            if (outfilename != "")
+            {
+                if (outputDocument.PageCount != 0)
+                {
+                    outputDocument.Save(outfilename);
+                    MessageBox.Show("PDF creationed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Output PDF has no pages, nothing to save", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please provide Output FileName!", "Warning",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // Displays a SaveFileDialog so the user can save the pdf
+            // assigned to Button2.
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "PDF file|*.pdf";
+            saveFileDialog1.Title = "Save the Output File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string display the result name in textbox for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                textBox2.Text = saveFileDialog1.FileName;
+            }
+
+
+        }
+
+
+        
+     }
+    
+}
